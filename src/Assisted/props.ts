f00ff078af368d37c4efadcdc09a97b85608990e
Andres Babativa <carlos3k11@gmail.com>
2024-02-14 10:11:34 -0500

    feat: implement the Assisted component

---
+++ src/Assisted/props.ts
@@ -0,0 +1,37 @@
+export const parameters = {
+  docs: {
+    descriptions: {
+      component:
+        "The assited displays the steps and progress through a journey.",
+    },
+  },
+};
+
+export const props = {
+  steps: {
+    description:
+      "(Array of objects): An array to represent each step of the journey. Each object in the array represents one step and must have the following structure: id, label, description (Optional). The order of the steps depends on the order in the array,",
+  },
+  currentStepId: {
+    description:
+      "(string | number): An identifier that matches one of the id values within the steps array to indicate the current step.",
+  },
+
+  handlePrev: {
+    description:
+      "(Function): (Optional) A function that will be called when the user clicks on the previous button. If not provided, the button will not be rendered.",
+  },
+
+  handleNex: {
+    descriptions:
+      "(Function): (Optional) A function that will be called when the user clicks on the next button.",
+  },
+  titleButtonBefore: {
+    description:
+      "(string): (Optional) A string to be displayed in the button before the label of the current step.",
+  },
+  titleButtonAfter: {
+    description:
+      "(string): (Optional) A string to be displayed in the button after the label of the current step.",
+  },
+};
---
+++ src/Assisted/styles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { inube } from "@inubekit/foundations";
+
+export const StyledProgressBar = styled.div`
+  border-radius: 10px;
+  transition: width 0.5s;
+  height: 16px;
+  width: 100%;
+  background-color: ${({ theme }) =>
+    theme?.color?.surface?.dark?.clear || inube.color.surface.dark.clear};
+`;
+
+export const StyledProgressIndicator = styled.div`
+  border-radius: 8px;
+  transition: width 0.5s;
+  height: 16px;
+  width: ${({ $arrayLength, $currentStep }) =>
+    `${($currentStep / $arrayLength) * 100}%`};
+  background: ${({ theme }) =>
+    theme?.color?.surface?.primary?.regular ||
+    inube.color.surface.primary?.regular};
+`;
+
+export const StyledStepIndicator = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 20px;
+  height: 20px;
+  align-items: center;
+  border-radius: 50%;
+  border-width: 2px;
+  border-style: solid;
+  margin-bottom: ${inube.spacing.s100};
+  border-color: ${({ theme }) =>
+    theme?.color?.stroke?.primary?.regular ||
+    inube.color.stroke.primary?.regular};
+`;
